87b131cb8378a9a5d4f889eff0a8777d0a195abe
Gi <160989051+Giolii@users.noreply.github.com>
2024-04-20 15:12:48 -0700

    Updated grid in css

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('#container');
+
+
+const div1 = document.createElement('div');
+container.appendChild.getElementsByTagName(div);
+
+const div2 = document.createElement('div');
+
+const div3 = document.createElement('div');
+
+
+const div4 = document.createElement('div');
+
+
+const div5 = document.createElement('div');
+
+
+const div6 = document.createElement('div');
+
+
+container.appendChild.getElementsByTagName('div');
