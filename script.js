7c665f0a4f08112e07db026fb51e44a40bd73836
Gi <160989051+Giolii@users.noreply.github.com>
2024-04-21 13:29:34 -0700

    Added button for change number of squares

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const container = document.querySelector('#container');
+let column = 16;
+function buildSquares(){
+for (i=0; i < column*column ; i++){
+    const div = document.createElement('div');
+    container.appendChild(div);
+    div.style.width = `${500 / column}px`
+    div.style.height = `${500/column}px`;
+    div.addEventListener("mouseover", () => div.style.backgroundColor = 'black');    
+}
+}
 
-
-const div1 = document.createElement('div');
-container.appendChild.getElementsByTagName(div);
-
-const div2 = document.createElement('div');
-
-const div3 = document.createElement('div');
-
-
-const div4 = document.createElement('div');
-
-
-const div5 = document.createElement('div');
-
-
-const div6 = document.createElement('div');
-
-
-container.appendChild.getElementsByTagName('div');
+let square = document.querySelector('.square');
+square.addEventListener('click', () => {
+    column = Math.sqrt(prompt('How many squares do you want?'))
+    console.log(column);
+    buildSquares();
+});
+buildSquares();
