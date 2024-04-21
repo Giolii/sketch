855df1612d9e93d99b66c530a870e3db2035ebdb
Gi <160989051+Giolii@users.noreply.github.com>
2024-04-21 15:00:39 -0700

    Display a random color each square

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ buildSquares(column);
 
 function buildSquares(column){
 for (i=0; i < column*column ; i++){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
     const div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
     div.style.width = `${500 / column}px`;
     div.style.height = `${500 / column}px`;
-    div.addEventListener("mouseover", () => div.style.backgroundColor = squareColor);
+    div.addEventListener("mouseover", () => div.style.backgroundColor = `black`);
+    buttonColor = document.querySelector('.buttonColor');
+    buttonColor.addEventListener('click', function random(){
+    div.addEventListener("mouseover", () => div.style.backgroundColor = `#${randomColor}`);
+    })
 }
 }
 
@@ -25,9 +30,3 @@ buttonSquare.addEventListener('click', function changeSquare() {
     }while(isNaN(column) || column > 100 || column < 1);
     buildSquares(column);
 });
-
-buttonColor = document.querySelector('.buttonColor');
-buttonColor.addEventListener('click', function randomColor(){
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    squareColor = `#${randomColor}`;
-})
